1f8f1eda89266167b1b601968aa1789bc21ad6c9
Muhammed Rahif <rahifpalliyalil@gmail.com>
2023-03-17 21:14:45 +0530

    style(lint): fix eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,14 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:react/recommended", "standard-with-typescript"],
+  extends: ['plugin:react/recommended', 'standard-with-typescript', 'prettier'],
   overrides: [],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ["react"],
-  rules: {},
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
 };
