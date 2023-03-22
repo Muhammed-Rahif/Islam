fcd9a14c9b9e3b6f0dc10a3e779aa42a8d5cd105
Muhammed Rahif <rahifpalliyalil@gmail.com>
2023-03-22 11:32:01 +0530

    feat(component): create 'motioncaret' component

---
+++ src/components/MotionCaret.tsx
@@ -0,0 +1,43 @@
+import { IonIcon, useIonViewDidEnter } from '@ionic/react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { caretBackOutline } from 'ionicons/icons';
+import { useEffect, useRef } from 'react';
+
+interface MotionCaretProps {
+  show: boolean;
+  title?: string;
+}
+
+const MotionCaret: React.FC<MotionCaretProps> = ({ show, title }) => {
+  const ref = useRef<HTMLSpanElement>(null);
+
+  useEffect(() => {
+    if (!ref.current?.offsetTop) return;
+    const ionContent = document.querySelector('ion-content');
+    // console.log(ionContent, ref.current.offsetTop);
+    ionContent?.scrollToPoint(0, ref.current.offsetTop, 500);
+  }, []);
+
+  return (
+    <AnimatePresence>
+      {show && (
+        <motion.span
+          ref={ref}
+          title={title}
+          initial={{ width: '0rem', x: 1, opacity: 0 }}
+          animate={{ width: '1.5rem', x: 0, opacity: 1 }}
+          exit={{ width: '0rem', x: 1, opacity: 0 }}
+          className="inline-block"
+        >
+          <IonIcon
+            className="w-6 h-6"
+            icon={caretBackOutline}
+            color="primary"
+          />
+        </motion.span>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default MotionCaret;
--- src/features/view-chapter/components/ReadingContent.tsx
+++ src/features/view-chapter/components/ReadingContent.tsx
@@ -4,6 +4,9 @@ import { Fragment } from 'react';
 import { useParams } from 'react-router-dom';
 import { numToArabic } from 'utils/string';
 import { useVersesUthmani } from '../api/useVersesUthmani';
+import MotionCaret from 'components/MotionCaret';
+import { useAtom } from 'jotai/react';
+import { quranLastReadAtom } from 'stores/quranLastRead';
 
 const ReadingContent: React.FC = () => {
   const { id } = useParams<{ id: string }>();
@@ -14,6 +17,7 @@ const ReadingContent: React.FC = () => {
   } = useVersesUthmani({
     chapterId: parseInt(id),
   });
+  const [lastRead, setLastRead] = useAtom(quranLastReadAtom);
 
   return (
     <div className="[direction:rtl] leading-9 text-justify my-3">
@@ -41,7 +45,26 @@ const ReadingContent: React.FC = () => {
         <span className="mb-2 text-justify">
           {versesUthmaniData?.verses.map((verse, indx) => (
             <Fragment key={indx}>
-              {verse?.text_uthmani} {`  ﴿${numToArabic(++indx)}﴾  `}
+              <MotionCaret
+                title="You last read this verse"
+                show={
+                  lastRead?.reading?.chapterId.toString() === id &&
+                  lastRead?.reading?.verseId === verse?.id
+                }
+              />
+              <span
+                onDoubleClick={(e) => {
+                  setLastRead({
+                    translation: lastRead?.translation,
+                    reading: {
+                      chapterId: parseInt(id),
+                      verseId: verse?.id,
+                    },
+                  });
+                }}
+              >
+                {verse?.text_uthmani} {`  ﴿${numToArabic(++indx)}﴾  `}
+              </span>
             </Fragment>
           ))}
         </span>
