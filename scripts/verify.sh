475a236b102c58e2bbef4347fbd77432680d7fa2
Muhammed Rahif <rahifpalliyalil@gmail.com>
2023-03-24 15:18:08 +0530

    fix(scripts): verify.sh updated

--- scripts/verify.sh
+++ scripts/verify.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-node scripts/versioning.js -v $1 
-CI=false 
-yarn build 
-npx cap sync android 
-ionic capacitor update 
-ionic capacitor copy android 
-cd android 
-chmod +x gradlew 
-./gradlew assembleDebug 
-./gradlew assembleRelease 
-cd .. 
-CI=true
-exit 0
+node scripts/versioning.js -v $1 && exit 0
+CI=false && exit 0
+yarn build && exit 0
+npx cap sync android && exit 0
+ionic capacitor update && exit 0
+ionic capacitor copy android && exit 0
+cd android && exit 0
+chmod +x gradlew && exit 0
+./gradlew assembleDebug && exit 0
+./gradlew assembleRelease && exit 0
+cd .. && exit 0
+CI=true && exit 0
