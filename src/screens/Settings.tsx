f57e996bd3bfd1c0583ce94ea6e46655ad95225b
Muhammed Rahif <rahifpalliyalil@gmail.com>
2023-03-26 14:14:48 +0530

    fix(component): make general settings saperate

--- src/screens/Settings.tsx
+++ src/screens/Settings.tsx
@@ -15,16 +15,9 @@ import { THEME_KEY } from 'config/store';
 import { QuranSettings } from 'features/settings';
 import { useLocalStorage } from 'hooks/useLocalStore';
 import { useEffect } from 'react';
+import GeneralSettings from '../features/settings/components/GeneralSettings';
 
 const Settings: React.FC = () => {
-  const [theme, setTheme] = useLocalStorage(THEME_KEY, 'light');
-
-  useEffect(() => {
-    theme === 'dark'
-      ? document.body.classList.add('dark')
-      : document.body.classList.remove('dark');
-  }, [theme]);
-
   return (
     <IonPage>
       <IonHeader>
@@ -34,26 +27,7 @@ const Settings: React.FC = () => {
       </IonHeader>
       <IonContent fullscreen scrollY={false} className="ion-padding">
         <IonList>
-          <IonItemGroup className="pb-3">
-            <h2 className="text-xl font-bold mb-2">General</h2>
-            <div className="flex justify-between items-center h-9">
-              <IonText>
-                <h2 className="text-base">Theme</h2>
-              </IonText>
-              <IonSelect
-                interface="popover"
-                placeholder="Select theme"
-                className="py-0"
-                value={theme}
-                onIonChange={(e) => setTheme(e.detail.value)}
-              >
-                <IonSelectOption value="dark">Dark</IonSelectOption>
-                <IonSelectOption value="light">Light</IonSelectOption>
-              </IonSelect>
-            </div>
-            <IonItemDivider className="min-h-[2px] my-2" />
-          </IonItemGroup>
-
+          <GeneralSettings />
           <QuranSettings />
         </IonList>
       </IonContent>
