21866ab4d7ec1bb391e43c13ac52efb46f782f62
Muhammed Rahif <rahifpalliyalil@gmail.com>
2023-03-29 07:09:40 +0530

    chore(semantic-release): versioning.js update

--- scripts/build/versioning.js
+++ scripts/build/versioning.js
@@ -1,25 +1,39 @@
 // when running this file version argument should be passed as "-v v1.0.0"
 const { exec } = require('child_process');
+const fs = require('fs');
 
 const preCommit = async () => {
   console.log('====================================');
 
   const args = process.argv.slice(2);
-  console.log('Precommit with args: ', args);
+  console.log('Versioning precommit with args: ', args);
 
   const version = args[1];
-  console.log('Precommit with version string: ', version);
+  console.log('Versioning precommit with version string: ', version);
 
   const buildNo = version.replaceAll('v', '').match(RegExp(/^\d+/g))[0];
-  console.log('Precommit with build no: ', buildNo);
-
-  console.log('====================================');
+  console.log('Versioning precommit with build no: ', buildNo);
 
   await new Promise((resolve, reject) => {
+    fs.readFile('./package.json', (err, data) => {
+      if (err) throw err;
+
+      var packageJsonObj = JSON.parse(data);
+      packageJsonObj.version = version;
+      packageJsonObj = JSON.stringify(packageJsonObj);
+
+      fs.writeFile('./package.json', packageJsonObj, (err) => {
+        if (err) throw err;
+        console.log(`Updated package.json version to ${version}`);
+      });
+    });
+
     exec(
-      `yarn capacitor-set-version . -v ${version} -b ${buildNo} --json`,
+      `yarn prettier --write package.json && yarn capacitor-set-version . -v ${version} -b ${buildNo} --json`,
       (err, stdout) => {
-        console.log(stdout);
+        console.log(
+          `Versioning precommit successful with version: ${version} and build no: ${buildNo}`
+        );
         if (err) {
           reject(err);
           return;
@@ -28,6 +42,8 @@ const preCommit = async () => {
       }
     );
   });
+
+  console.log('====================================');
 };
 
 preCommit();
