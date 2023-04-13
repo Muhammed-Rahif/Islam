cbc1c1cf4f14977e9b27f9248019b15a0e2c6b39
Muhammed Rahif <rahifpalliyalil@gmail.com>
2023-04-13 16:48:39 +0530

    refactor: update exports of features

--- src/features/list-prayer-times/index.ts
+++ src/features/list-prayer-times/index.ts
@@ -2,4 +2,3 @@ export * from './types/PrayerTimesResponse';
 export * from './api/usePrayerTimes';
 export * from './components/ListPrayerTimes';
 export * from './utils/prayerTimes';
-export * from '../settings/types/PrayerMethodsResponse';
